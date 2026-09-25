f360c4ceabed0b431c0742c5e5aaf19c99b972cf
vadicherlanikhil001-boop <vadicherla.nikhil001@gmail.com>
2026-09-25 17:11:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 import joblib
-
 app = FastAPI()
-
-model = joblib.load("mymodel_1")
-
-templates = Jinja2Templates(directory="templates")
-
-
 @app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={
-            "result": None,
-            "url": None
-        }
-    )
-
-
-@app.post("/predict")
-async def predict_iris(
-    request: Request,
-    sl: float = Form(...),
-    sw: float = Form(...),
-    pl: float = Form(...),
-    pw: float = Form(...)
-):
-
-    result = model.predict([[sl, sw, pl, pw]])[0]
-
-    url = "https://daylily-phlox.eu/wp-content/uploads/2021/10/Iris-setosa-dwarf-form.jpg"
-
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={
-            "result": int(result),
-            "url": url
-        }
-    )
+def predict_iris(sl:float,sw:float,pl:float,pw:float):
+  """functtion to predict IRIS data"""
+  model = joblib.load("mymodel")
+  result = model.predict([[sl,sw,pl,pw]])
+  return {"predcition is ":int(result[0])}
+  
